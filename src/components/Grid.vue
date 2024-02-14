6db6ec782ba43cf195f38f09dcddfed59ca1a4db
robertkelm <kelm@rackow-berlin.de>
2024-02-14 15:00:06 +0100

    Add Grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,9 +1,26 @@
 <script setup>
 
+import GridCell from "@/components/GridCell.vue";
 </script>
 
 <template>
-
+  <div class="container">
+    <div class="row">
+      <GridCell/>
+      <GridCell/>
+      <GridCell/>
+    </div>
+    <div class="row">
+      <GridCell/>
+      <GridCell/>
+      <GridCell/>
+    </div>
+    <div class="row">
+      <GridCell/>
+      <GridCell/>
+      <GridCell/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
