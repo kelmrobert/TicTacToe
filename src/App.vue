7fa21109a0761004a7b3f238a5f4b57695aed0d2
robertkelm <kelm@rackow-berlin.de>
2024-02-14 18:40:45 +0100

    Remove the bullshit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Grid from "@/components/Grid.vue";
 
 const winCombinations = {
   0: [1,2,3],
@@ -12,32 +11,10 @@ const winCombinations = {
   7: [3,5,7],
 }
 
-let xValues = [1,2,3]
-let oValues = [4,5,6]
-let player1 = true
-
-const handleCellClick = (id) => {
-  console.log("Klick auf Zelle " + id)
-  update(id)
-}
-
-function update(id) {
-  console.log(xValues)
-  if (player1) {
-    xValues.push(id)
-  } else {
-    oValues.push(id)
-  }
-  player1 = !player1
-  console.log(xValues)
-}
-
 </script>
 
 <template>
-  <h4 v-if="player1">Spieler 1 ist am Zug</h4>
-  <h4 v-if="!player1">Spieler 2 ist am Zug</h4>
-  <Grid :xValues="xValues" :oValues="oValues" @cellClicked="handleCellClick"></Grid>
+
 </template>
 
 <style scoped>
