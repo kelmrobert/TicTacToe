a0a7d73383efc176cc6dcf753889034892d106ff
robertkelm <kelm@rackow-berlin.de>
2024-02-14 15:17:19 +0100

    Add grid cells to grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -5,24 +5,29 @@ import GridCell from "@/components/GridCell.vue";
 
 <template>
   <div class="container">
-    <div class="row">
-      <GridCell/>
-      <GridCell/>
-      <GridCell/>
-    </div>
-    <div class="row">
-      <GridCell/>
-      <GridCell/>
-      <GridCell/>
-    </div>
-    <div class="row">
-      <GridCell/>
-      <GridCell/>
-      <GridCell/>
+    <div class="row justify-content-center">
+      <div class="col-4">
+        <div class="grid-container">
+          <GridCell/>
+          <GridCell/>
+          <GridCell/>
+          <GridCell/>
+          <GridCell/>
+          <GridCell/>
+          <GridCell/>
+          <GridCell/>
+          <GridCell/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 
+.grid-container {
+  display: inline-grid;
+  grid-template-columns: auto auto auto;
+  padding: 40px;
+}
 </style>
