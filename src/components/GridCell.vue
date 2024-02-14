7f4b9e2a213a29e3b5a1fab1d8f88ce6062ac89c
robertkelm <kelm@rackow-berlin.de>
2024-02-14 14:59:58 +0100

    Init GridCell

--- src/components/GridCell.vue
+++ src/components/GridCell.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-
+  <div class="col grid-cell">ICON HERE</div>
 </template>
 
 <style scoped>
-
+  .grid-cell {
+    border: 1px solid darkslategrey;
+    height: 100px;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 100px;
+  }
 </style>
