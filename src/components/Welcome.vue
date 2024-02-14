1111ef3f25d28e888020cce1ed662075db0e64a3
robertkelm <kelm@rackow-berlin.de>
2024-02-14 22:54:30 +0100

    Add margin

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -9,9 +9,13 @@
 </script>
 
 <template>
-    <h1 class="text-primary-emphasis">Ready to Loose?</h1>
-    <button class="btn btn-outline-success">Spiel starten</button>
-    <button class="btn btn-outline-danger" @mouseover="changeLocation">Hilfe</button>
+    <div>
+        <h1 class="text-primary-emphasis">Ready to Loose?</h1>
+    </div>
+    <div class="mt-5">
+        <button class="btn btn-outline-success">Spiel starten</button>
+        <button class="btn btn-outline-danger" @mouseover="changeLocation">Hilfe</button>
+    </div>
 </template>
 
 <style scoped>
