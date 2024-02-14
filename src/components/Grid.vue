4a3d070499336d77cef38a991eb81d75f8f7a0e3
robertkelm <kelm@rackow-berlin.de>
2024-02-14 19:02:45 +0100

    Display grid

---
+++ src/components/Grid.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import GridCell from "@/components/GridCell.vue";
+</script>
+
+<template>
+    <div class="grid" >
+        <GridCell v-for="n in 9" :id="n"/>
+    </div>
+</template>
+
+<style scoped>
+    .grid {
+        display: grid;
+        gap: 1px;
+        grid-template-columns: repeat(3, 1fr);
+    }
+</style>
---
+++ src/components/GridCell.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import Cross from "@/components/Cross.vue";
+</script>
+
+<template>
+    <div class="square">
+        <Cross />
+    </div>
+</template>
+
+<style scoped>
+    .square {
+        width: 100px;
+        height: 100px;
+        border: 2px solid gray;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
