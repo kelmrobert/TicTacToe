eb05f5141a6a9d9f82026642c0c5471babbd18ae
robertkelm <kelm@rackow-berlin.de>
2024-02-14 19:18:20 +0100

    Fleeing button

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,9 +1,17 @@
 <script setup>
 
+    function changeLocation(event){
+        let button = event.target;
+        button.style.position = 'absolute';
+        button.style.left = Math.random() * window.innerWidth + 'px'
+        button.style.top = Math.random() * window.innerHeight + 'px'
+    }
 </script>
 
 <template>
     <h1 class="text-primary-emphasis">Ready to Loose?</h1>
+    <button class="btn btn-outline-success">Ich versuche mein Glück</button>
+    <button class="btn btn-outline-danger" @mouseover="changeLocation">Hilfe</button>
 </template>
 
 <style scoped>
