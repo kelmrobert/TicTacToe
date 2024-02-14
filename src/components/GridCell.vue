73c89bda05e17090dc3609ae44886f72ee9725c2
robertkelm <kelm@rackow-berlin.de>
2024-02-14 15:17:09 +0100

    Create grid cell

--- src/components/GridCell.vue
+++ src/components/GridCell.vue
@@ -3,15 +3,16 @@
 </script>
 
 <template>
-  <div class="col grid-cell">ICON HERE</div>
+  <div class="grid-item">ICON HERE</div>
 </template>
 
 <style scoped>
-  .grid-cell {
-    border: 1px solid darkslategrey;
-    height: 100px;
+  .grid-item {
+    padding: 20px;
     text-align: center;
     vertical-align: middle;
-    line-height: 100px;
+    border: 1px solid black;
+    min-height: 100px;
+    min-width: 100px;
   }
 </style>
