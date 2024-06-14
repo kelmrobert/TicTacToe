9f87a527d9a5d698b1cee9dc04eb0cd4fea37360
robertkelm <kelm@rackow-berlin.de>
2024-06-14 13:18:04 +0200

    Test commit

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -8,7 +8,7 @@
 
 <template>
     <h2 v-if="props.player1" class="mb-5">Robert ist am Zug!</h2>
-    <h2 v-if="!props.player1" class="mb-5">Spieler 2 ist am Zug!</h2>
+    <h2 v-if="!props.player1" class="mb-5">Spieler 2 ist am Zug!!</h2>
 </template>
 
 <style scoped>
